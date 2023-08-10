c20ffaa96e0d77abee1a0edf5acea5375875941a
Anastasia <anadvoryanchikova@gmail.com>
2023-08-10 11:37:38 +0200

    connection to graphdb

--- data_to_db.py
+++ data_to_db.py
@@ -8,8 +8,6 @@
 """
 import django
 import os
-from django.core.management import BaseCommand
-from csv import DictReader
 import pandas as pd
 import numpy as np
 
@@ -50,9 +48,7 @@ def clean_regulation_table(csv_file_regulation):
 
     df_regulation["date_effect"] = df_regulation["date_effect"].replace("Not found", np.nan)
     df_regulation["date_sig"] = df_regulation["date_sig"].replace("Not found", np.nan)
-
     df_regulation["date_deadline"] = df_regulation["date_deadline"].replace("Not found", np.nan)
-    # print(df_regulation.dtypes)
 
     df_regulation["publication_date"] = pd.to_datetime(df_regulation["publication_date"], format = "%d/%m/%Y")
     df_regulation["date_effect"] = pd.to_datetime(df_regulation["date_effect"], format = "%d/%m/%Y")
@@ -60,16 +56,9 @@ def clean_regulation_table(csv_file_regulation):
     df_regulation["date_deadline"] = pd.to_datetime(df_regulation["date_deadline"], format = "%d/%m/%Y")
 
     df_regulation["publication_date"] = df_regulation["publication_date"].dt.strftime("%Y-%m-%d")
-    # df_regulation["publication_date"] = df_regulation["publication_date"].astype("string")
-
     df_regulation["date_effect"] = df_regulation["date_effect"].dt.strftime("%Y-%m-%d")
-    # df_regulation["date_effect"] = df_regulation["date_effect"].astype("string")
-
     df_regulation["date_sig"] = df_regulation["date_sig"].dt.strftime("%Y-%m-%d")
-    # df_regulation["date_sig"] = df_regulation["date_sig"].astype("string")
-
     df_regulation["date_deadline"] = df_regulation["date_deadline"].dt.strftime("%Y-%m-%d")
-    # df_regulation["date_deadline"] = df_regulation["date_deadline"].astype("string")
     
     df_regulation = df_regulation.replace([pd.NaT], [None])
     df_regulation.to_csv("./data/cleaned_primary_secondary_regulations.csv", index = False)
@@ -88,14 +77,38 @@ def clean_regulation_table(csv_file_regulation):
 
 
 # Either doc_code to give OR source__doc_code = row[source]
+obj = RegulationRelation.objects.all()
+obj.delete()
+
 df_regulations = clean_regulation_table("./data/primary_secondary_regulations.csv")
 df_relations = pd.read_csv("./data/full_df.csv")
 df_relations = df_relations.drop_duplicates()
 
-for _, row in df_relations.iterrows():
-    for i in range(len(df_regulations)):
-        if row.source in df_regulations.loc[i, "doc_code"] and row.target in df_regulations.loc[i, "doc_code"]:
-            print(row.source, row.target)
-    # regulation=Regulation(source__doc_code =row['source'], target__doc_code=row['target'], 
-    #                     name=row['name'])  
-    # regulation.save()
+def merge_datframes(regulation, relation):
+    regulation = regulation.drop("Unnamed: 0", axis = 1)
+    regulation = regulation.rename(columns = {"doc_code": "source"})
+    df1 = relation[["source", "target", "relation"]].merge(regulation[["source"]], on =  "source", how = "inner")
+
+    regulation = regulation.rename(columns = {"source":"target"})
+    final_df = df1[["source", "target", "relation"]].merge(regulation[["target"]], on = "target", how = "inner")
+    final_df["relation"] = final_df["relation"].astype("string")
+    final_df["source"] = final_df["source"].astype("string")
+    final_df["target"] = final_df["target"].astype("string")
+
+    return final_df
+
+final_df = merge_datframes(df_regulations, df_relations)
+print(final_df.dtypes)
+
+for _, row in final_df.iterrows():
+    source_object = Regulation.objects.get(doc_code=row["source"])
+    target_object = Regulation.objects.get(doc_code=row["target"])
+    relation = row["relation"]
+
+    usershipping, created = RegulationRelation.objects.get_or_create(
+        source=source_object,
+        target = target_object,
+        relation_type = relation
+    )
+    usershipping.save()
+
